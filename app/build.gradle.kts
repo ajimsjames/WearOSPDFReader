025e48847ba9eec997fb618e6744f1a8ebb3941a
Aju George <ajimsjames@gmail.com>
2026-07-28 16:07:12 +0530

    Release v2.2.0: Set correct versionCode to 9 and versionName to 2.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.ajimsjames.wearpdfreader"
         minSdk = 30
         targetSdk = 33
-        versionCode = 14
+        versionCode = 9
         versionName = "2.2.0"
 
         vectorDrawables {
